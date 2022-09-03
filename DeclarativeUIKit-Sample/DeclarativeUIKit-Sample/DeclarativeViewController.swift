2d4e3e92c0d5593557008620abe59fc476492d06
kentaro kimoto <exiler2411k@gmail.com>
2022-09-03 13:04:44 +0900

    fix

--- DeclarativeUIKit-Sample/DeclarativeUIKit-Sample/DeclarativeViewController.swift
+++ DeclarativeUIKit-Sample/DeclarativeUIKit-Sample/DeclarativeViewController.swift
@@ -7,75 +7,52 @@
 
 import UIKit
 import DeclarativeUIKit
-import SwiftUI
+
+
+extension Notification.Name {
+    static let injection = Notification.Name("INJECTION_BUNDLE_NOTIFICATION")
+}
+
+extension NotificationCenter {
+    func addInjectionObserver(_ observer: Any, selector: Selector, object: Any?) {
+        NotificationCenter.default.addObserver(observer, selector: selector, name: .injection, object: object)
+    }
+}
 
 
 final class DeclarativeViewController: UIViewController {
     
-    override func viewDidLoad() {
-           super.viewDidLoad()
-           
-           self.view.backgroundColor = .white
-
-           self.declarative {
-               UIScrollView.vertical {
-                   UIStackView.vertical {
-
-                       UIView()
-                         .imperative { view in
-                             print("命令的に記述も可能")
-                             view.tintColor = .black
-                             view.isUserInteractionEnabled = true
-                         }
-                         .size(width: 100, height: 100)
-                         .backgroundColor(.blue)
-                         .transform(.init(rotationAngle: 45.0 / 360 * Double.pi))
-                         .cornerRadius(30, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMaxYCorner])
-                         .border(color: .blue, width: 10)
-                         .customSpacing(40)
-
-                       UIButton()
-                           .title("button", for: .normal)
-                           .titleColor(.brown, for: .normal)
-                           .add(target: self, for: .touchUpInside, { _ in
-                               print("タッチアクション")
-                           })
-
-                       Array(1 ... 10).compactMap { num in
-                           UILabel("\(num)番目のlabel")
-                               .textColor(.black)
-                               .textAlignment(.center)
-                       }
-                       
-                       UISwitch()
-                           .add(target: self, for: .allEvents, { _ in
-                               print("hoge")
-                           })
-
-                       UIView.spacer()
-                   }
-                   .spacing(20)
-               }
-           }
-       }
-}
-
-struct DeclarativeViewController_Wrapper: UIViewControllerRepresentable {
-    typealias ViewController = DeclarativeViewController
-
-    func makeUIViewController(context: Context) -> ViewController {
-        let vc = DeclarativeViewController()
-        return vc
-    }
     
-    func updateUIViewController(_ vc: ViewController, context: Context) {
+    override func loadView() {
+        super.loadView()
+        NotificationCenter.default.addInjectionObserver(self, selector: #selector(setupLayout), object: nil)
+        setupLayout()
     }
 }
 
-struct DeclarativeViewController_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            DeclarativeViewController_Wrapper()
+@objc private extension DeclarativeViewController {
+    func setupLayout() {
+        //初期時に画面を白にしておく
+        self.view.backgroundColor = .white
+        
+        self.declarative {
+            UIStackView.vertical {
+                UILabel()
+                    .imperative { label in
+                    let label = label as! UILabel
+                    label.text = "hogehoge"
+                }
+                .backgroundColor(.gray)
+                .height(100)
+                
+                UIView()
+                    .imperative { view in
+                        view.tintColor = .black
+                        view.isUserInteractionEnabled = true
+                    }
+                UIView.spacer()
+                    .backgroundColor(.brown)
+            }
         }
     }
 }
---
+++ DeclarativeUIKit-Sample/DeclarativeUIKit-Sample/InjectionObserver.swift
@@ -0,0 +1,47 @@
+//
+//  InjectionObserver.swift
+//  DeclarativeUIKit-Sample
+//
+//  Created by 木元健太郎 on 2022/08/24.
+//
+
+import UIKit
+import SwiftUI
+
+#if DEBUG
+import Combine
+
+public let injectionObserver = InjectionObserver()
+
+public class InjectionObserver: ObservableObject {
+    @Published var injectionNumber = 0
+    var cancellable: AnyCancellable? = nil
+    let publisher = PassthroughSubject<Void, Never>()
+    init() {
+        cancellable = NotificationCenter.default.publisher(for:
+            Notification.Name("INJECTION_BUNDLE_NOTIFICATION"))
+            .sink { [weak self] change in
+            self?.injectionNumber += 1
+            self?.publisher.send()
+        }
+    }
+}
+
+extension View {
+    public func eraseToAnyView() -> some View {
+        return AnyView(self)
+    }
+    public func onInjection(bumpState: @escaping () -> ()) -> some View {
+        return self
+            .onReceive(injectionObserver.publisher, perform: bumpState)
+            .eraseToAnyView()
+    }
+}
+#else
+extension View {
+    public func eraseToAnyView() -> some View { return self }
+    public func onInjection(bumpState: @escaping () -> ()) -> some View {
+        return self
+    }
+}
+#endif
